35fbfdf29ecad50cdcc9bc9011eb693944ceb552
shapegroup <>
2021-06-08 10:47:26 +0200

    ...

--- resources/libraries/k-core/core.dev.js
+++ resources/libraries/k-core/core.dev.js
@@ -13,7 +13,7 @@ const ui = (() => {
 
         function debug(){ console.debug.apply(console,arguments); }
 
-        debug(`:: [🛈 Version] Kimera V2.8.30f3d - VisorBeta`);
+        debug(`:: [🛈 Version] Kimera V2.8.30f3e - VisorBeta`);
         debug(`:: [🛈 Project] https://git.io/JIJEt`);
         debug(`:: [🛈 wikizone] https://git.io/fhSzk`);
         debug(`:: [🛈 licence] GNU V3 https://git.io/JJVw0`);
@@ -10644,10 +10644,10 @@ const ui = (() => {
                             {
                                 isfirstplay=false;
                                 audio.muted=false;
+                                audio.volume=1.0;
                                 audio.removeAttribute("muted");
-                                console.log("VOLUME SET!");
                                 audio.setAttribute('volume',1.0)
-                                audio.volume = 1.0;
+                                console.log("VOLUME SET!");
 
                             }
 
