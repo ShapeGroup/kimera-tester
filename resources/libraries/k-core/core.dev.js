a6c7fb637e74bbc5df4eee4f184f855040e8c948
shapegroup <>
2021-06-08 08:59:15 +0200

    ...

--- resources/libraries/k-core/core.dev.js
+++ resources/libraries/k-core/core.dev.js
@@ -13,7 +13,7 @@ const ui = (() => {
 
         function debug(){ console.debug.apply(console,arguments); }
 
-        debug(`:: [🛈 Version] Kimera V2.8.29h5 - VisorBeta`);
+        debug(`:: [🛈 Version] Kimera V2.8.29h5b - VisorBeta`);
         debug(`:: [🛈 Project] https://git.io/JIJEt`);
         debug(`:: [🛈 wikizone] https://git.io/fhSzk`);
         debug(`:: [🛈 licence] GNU V3 https://git.io/JJVw0`);
@@ -160,7 +160,7 @@ const ui = (() => {
             for(let v of videostag)
             {
 
-                if(!v.getAttribute('playsinline') || !v.getAttribute('muted') || v.muted != false) {
+                if( !v.getAttribute('playsinline') || !v.getAttribute('muted')) {
                     debug(`:: [⚠ ui alert]: Safari Wrong video asset\n   ⮑ Apple Safari "need playsinline" and "muted" attribute on all videos. Note: It's not possible to add dynamically via script.`, v);
                 }
 
