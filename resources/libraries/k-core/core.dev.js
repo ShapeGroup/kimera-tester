e4f6ffacf30a2e7f30d36b3914573bc8ac43fde9
Shape group <albertomara@mail.com>
2021-07-26 17:49:34 +0200

    ...

--- resources/libraries/k-core/core.dev.js
+++ resources/libraries/k-core/core.dev.js
@@ -3,7 +3,7 @@ const ui = (() => {
 
 
         /*
-        //	[ kimera framework V 2.8.32f11 ]
+        //	[ kimera framework V 2.8.32f11b ]
         //	Credits: Alberto Marà & Shape group
         //	https://github.com/ShapeGroup/kimera-frontend-framework/wiki
         //	https://www.facebook.com/kimeraframework/
@@ -12,7 +12,7 @@ const ui = (() => {
 
         function debug(){ console.debug.apply(console,arguments); }
 
-        debug(`:: [🛈 Version] V2.8.32f11 kimera`);
+        debug(`:: [🛈 Version] V2.8.32f11b kimera`);
         debug(`:: [🛈 Project] https://git.io/JIJEt`);
         debug(`:: [🛈 wikizone] https://git.io/fhSzk`);
         debug(`:: [🛈 licence] GNU V3 https://git.io/JJVw0`);
@@ -381,7 +381,8 @@ const ui = (() => {
 
                 if(lazyobserverlist.length>-1 || lazywhenviewlist.length>-1)
                 {
-					let winbody = window.document.body;
+
+					const winbody = window.document.body;
 
                     winbody.onscroll = ev_scrollpage => {
 
@@ -12125,7 +12126,7 @@ const ui = (() => {
             exitloader();
 
 			if(document.querySelectorAll('.k-version')[0])
-			document.querySelectorAll('.k-version')[0].innerHTML = "V2.8.32f11"
+			document.querySelectorAll('.k-version')[0].innerHTML = "V2.8.32f11b"
 
         })),false);
 
