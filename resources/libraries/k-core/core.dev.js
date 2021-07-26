4b0f7ebaa316583cad48668fe450a44299551d78
Shape group <albertomara@mail.com>
2021-07-26 16:13:46 +0200

    ...

--- resources/libraries/k-core/core.dev.js
+++ resources/libraries/k-core/core.dev.js
@@ -3,7 +3,7 @@ const ui = (() => {
 
 
         /*
-        //	[ kimera framework V 2.8.32e ]
+        //	[ kimera framework V 2.8.32f2 ]
         //	Credits: Alberto Marà & Shape group
         //	https://github.com/ShapeGroup/kimera-frontend-framework/wiki
         //	https://www.facebook.com/kimeraframework/
@@ -12,7 +12,7 @@ const ui = (() => {
 
         function debug(){ console.debug.apply(console,arguments); }
 
-        debug(`:: [🛈 Version] V2.8.32f kimera`);
+        debug(`:: [🛈 Version] V2.8.32f2 kimera`);
         debug(`:: [🛈 Project] https://git.io/JIJEt`);
         debug(`:: [🛈 wikizone] https://git.io/fhSzk`);
         debug(`:: [🛈 licence] GNU V3 https://git.io/JJVw0`);
@@ -198,7 +198,7 @@ const ui = (() => {
 
                     Loader.classList.add('[status-off]');
 
-                },10); // wait elements call
+                },25); // wait elements call
 
                 setTimeout(()=>{
 
@@ -383,8 +383,7 @@ const ui = (() => {
                 {
 
 
-
-                    document.documentElement.onscroll = ev_scrollpage => {
+                    document.body.onscroll = ev_scrollpage => {
 
                         let scrollpage = setInterval( ()=> {
 
@@ -12124,7 +12123,7 @@ const ui = (() => {
             exitloader();
 
 			if(document.querySelectorAll('.k-version')[0])
-			document.querySelectorAll('.k-version')[0].innerHTML = "V2.8.32f"
+			document.querySelectorAll('.k-version')[0].innerHTML = "V2.8.32f2"
 
         })),false);
 
